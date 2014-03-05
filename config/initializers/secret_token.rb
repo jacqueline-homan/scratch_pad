121c4b62e5bd2c0e099dc873d26ee0afb23de1b1
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-03-05 17:42:43 -0500

    Initial commit - removing turbolinks and adding backbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScratchPad::Application.config.secret_key_base = '06e9ef98397d3d691999f37053a240ada965f8e168ab45e0b7a137aabcaac454bd39b37438f3e567f1c49f1742b988963e57329e5ed73a1ab42b287e6db71b3d'
